215e2ad56a37fcf62859434fe6e672b4e79f91a4
Sparsha <203436764+sparsha-2006@users.noreply.github.com>
2026-02-14 20:53:31 +0530

    initial project setup with Vite, React, Tailwind and core folder structure

---
+++ versathon/learnix/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center text-3xl font-bold">
+      Learnix is Alive 🔥
+    </div>
+  )
+}
+
+export default App
